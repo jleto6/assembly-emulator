2f8206020a315094a71788b576b4d8e5162c23d5
jleto6 <jletobar@gmail.com>
2025-04-06 17:03:49 -0400

    Add initial implementation of SPIM core and example assembly files

---
+++ project.c
@@ -0,0 +1,91 @@
+#include "spimcore.h"
+
+/* ALU */
+/* 10 Points */
+void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
+{
+
+}
+
+/* instruction fetch */
+/* 10 Points */
+int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
+{
+  if (PC % 4 != 0) {
+	return 1;
+}
+if (PC < 0x4000) {
+	return 1;
+}
+unsigned index = (PC - 0x4000) >> 2;
+*instruction = Mem[index];
+return 0;
+}
+
+
+/* instruction partition */
+/* 10 Points */
+void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
+{
+	*op = (instruction >> 26) & 0x3F; //op code
+	*r1 = (instruction >> 21) & 0x1F; //rs 
+	*r2 = (instruction >> 16) & 0x1F; //rt
+	*r3 = (instruction >> 11) & 0x1F; //rd R-type
+	*funct = instruction & 0x3F; //function R-type
+	*offset = instruction & 0xFFFF; //address/immediate I-type
+	*jsec = instruction & 0x3FFFFFF; //target address J-type
+}
+
+
+
+/* instruction decode */
+/* 15 Points */
+int instruction_decode(unsigned op,struct_controls *controls)
+{
+
+}
+
+/* Read Register */
+/* 5 Points */
+void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
+{
+
+}
+
+
+/* Sign Extend */
+/* 10 Points */
+void sign_extend(unsigned offset,unsigned *extended_value)
+{
+
+}
+
+/* ALU operations */
+/* 10 Points */
+void ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
+{
+
+}
+
+/* Read / Write Memory */
+/* 10 Points */
+void rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem)
+{
+
+}
+
+
+/* Write Register */
+/* 10 Points */
+void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
+{
+
+}
+
+/* PC update */
+/* 10 Points */
+void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC)
+{
+
+}
+
